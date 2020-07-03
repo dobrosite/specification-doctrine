fec3c6cf0c1cdc190831a496d9c8662c4bcb9c57
Михаил Красильников <m.krasilnikov@webbankir.com>
2020-07-03 17:21:59 +0300

    Добавлена поддержка спецификации Any

---
+++ src/Handler/AnyHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DobroSite\Specification\Doctrine\Handler;
+
+use DobroSite\Specification\Any;
+use DobroSite\Specification\Doctrine\QueryBuilder\QueryBuilder;
+use DobroSite\Specification\Exception\Handler\UnsupportedSpecificationException;
+use DobroSite\Specification\Specification;
+use Doctrine\ORM\Query\Expr\Base;
+use Doctrine\ORM\Query\Expr\Comparison;
+use Doctrine\ORM\Query\Expr\Func;
+
+/**
+ * Обработчик спецификаций «Any».
+ *
+ * @since x.x
+ */
+class AnyHandler implements DoctrineHandler
+{
+    /**
+     * Создаёт условие на основе спецификации.
+     *
+     * @param Specification $specification Спецификация.
+     * @param QueryBuilder  $queryBuilder  Построитель запросов.
+     *
+     * @return Base|Comparison|Func|string
+     *
+     * @throws UnsupportedSpecificationException Если переданная спецификация не поддерживается.
+     */
+    public function createCondition(Specification $specification, QueryBuilder $queryBuilder)
+    {
+        if (!$specification instanceof Any) {
+            throw new UnsupportedSpecificationException($specification, $this);
+        }
+
+        return '1';
+    }
+
+    /**
+     * Возвращает имя класса поддерживаемых спецификаций.
+     *
+     * @return string
+     */
+    public function getSpecificationClassName(): string
+    {
+        return Any::class;
+    }
+}
---
+++ tests/Unit/Handler/AnyHandlerTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DobroSite\Specification\Doctrine\Tests\Unit\Handler;
+
+use DobroSite\Specification\Any;
+use DobroSite\Specification\Doctrine\Handler\AnyHandler;
+use DobroSite\Specification\Doctrine\QueryBuilder\QueryBuilder;
+use DobroSite\Specification\Exception\Handler\UnsupportedSpecificationException;
+use DobroSite\Specification\Specification;
+use Doctrine\ORM\Query\Expr;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Тесты обработчика спецификаций «Any».
+ *
+ * @covers \DobroSite\Specification\Doctrine\Handler\AnyHandler
+ */
+class AnyHandlerTest extends TestCase
+{
+    /**
+     * Проверяемый обработчик.
+     *
+     * @var AnyHandler
+     */
+    private $handler;
+
+    /**
+     * Составитель запросов.
+     *
+     * @var QueryBuilder&MockObject
+     */
+    private $queryBuilder;
+
+    /**
+     * Проверяет создание выражения из спецификации.
+     *
+     * @throws \Throwable
+     */
+    public function testCreateExpression(): void
+    {
+        $expr = $this->handler->createCondition(
+            new Any(),
+            $this->queryBuilder
+        );
+
+        self::assertEquals('1', (string) $expr);
+    }
+
+    /**
+     * Проверяет возврат обработчиком правильного имени спецификации.
+     *
+     * @throws \Throwable
+     */
+    public function testReturnValidSpecificationName(): void
+    {
+        self::assertEquals(Any::class, $this->handler->getSpecificationClassName());
+    }
+
+    /**
+     * Проверяет вбрасывание исключения при передаче неправильной спецификации.
+     *
+     * @throws \Throwable
+     */
+    public function testThrowExceptionOnInvalidSpecification(): void
+    {
+        $this->expectException(UnsupportedSpecificationException::class);
+        $this->expectExceptionMessageMatches(
+            '/.*AnyHandler supports only .*Any specifications, but Mock_Specification_\w+ given./'
+        );
+
+        $this->handler->createCondition(
+            $this->createMock(Specification::class),
+            $this->queryBuilder
+        );
+    }
+
+    /**
+     * Готовит окружение теста.
+     *
+     * @throws \Throwable
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->queryBuilder = $this->createConfiguredMock(
+            QueryBuilder::class,
+            ['expr' => new Expr()]
+        );
+
+        $this->handler = new AnyHandler();
+    }
+}
